1320c83aeb355d5b55b0392528de01deea34b80d
Kai Nguyen <snguyen@suncor.com>
2024-05-24 16:24:04 -0600

    Added images and checkbox

--- website/src/App.js
+++ website/src/App.js
@@ -1,15 +1,13 @@
 import React, { useState } from 'react';
 import './App.css';
-// import wilde_1 from './images/wilde_1.jpg';
-// import wilde_2 from './images/wilde_2.jpg';
-// import wilde_3 from './images/wilde_3.jpg';
-// import wilde_4 from './images/wilde_4.jpg';
+import photo_1 from './images/photo_1.jpg';
 
 const App = () => {
   const [showMessage, setShowMessage] = useState(true);
   const [showQuestion, setShowQuestion] = useState(false);
   const [showAnswer, setShowAnswer] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [selectedMovies, setSelectedMovies] = useState([]);
 
   const handleNext = () => {
     setShowMessage(false);
@@ -22,48 +20,78 @@ const App = () => {
     }
   };
 
-  // const handleNextImage = () => {
-  //   // Increment the current index, and loop back to 0 if reaching the end
-  //   setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-  // };
+  const handleNextImage = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
 
-  // const handlePrevImage = () => {
-  //   // Decrement the current index, and loop back to the last index if reaching the beginning
-  //   setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
-  // };
+  const handlePrevImage = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+  };
 
-  // const images = [wilde_1, wilde_2, wilde_3, wilde_4];
+  const handleCheckboxChange = (event) => {
+    const { value, checked } = event.target;
+    if (checked) {
+      setSelectedMovies((prevSelectedMovies) => [...prevSelectedMovies, value]);
+    } else {
+      setSelectedMovies((prevSelectedMovies) => prevSelectedMovies.filter(movie => movie !== value));
+    }
+  };
+
+  const images = [photo_1];
 
   return (
     <div className="App">
       {showMessage && (
         <div className="message">
-          <p className='name'>-insert name-, </p> 
-          <p>-insert message-</p>
-          <p>- -insert name-</p>
+          <p className='name'>Nicholas Kokorudz, </p> 
+          <p>You make my life so eventful and filled with joy. Spending time with you is never a dull moment. I appreciate how you can always find the 80s music for the moment with your cheeky comments. You're one of the kindest and most considerate people I know. I'm thankful that your family always being so nice to me. Thank you for being such a wonderful person and bringing joy into my life.</p>
+          <p>- Chen Qian</p>
           <button onClick={handleNext}>Next</button>
         </div>
       )}
       {showQuestion && (
         <div className="question">
-          <p>-insert question- ? 😊</p>
+          <p>Ready for your birthday gift? 😊</p>
           <div>
             <button className="yesButton" onClick={() => handleAnswer('Yes')}>Yes</button>
-            <button className="noButton">
-              No
-            </button>
+            <button className="noButton">No</button>
           </div>
         </div>
       )}
       {showAnswer && (
         <div className="answer">
-          <p>See you on -insert date- ❤️</p>
           <div className="image-container">
-            {/* <img src={images[currentIndex]} alt={`Wilde ${currentIndex + 1}`} />
+            <img src={images[currentIndex]} alt={`Wilde ${currentIndex + 1}`} />
             <div className="image-buttons">
               <button onClick={handlePrevImage}>&lt;</button>
               <button onClick={handleNextImage}>&gt;</button>
-            </div> */}
+            </div>
+          </div>
+          <div className="checklist">
+            <h3>See you on June 2, 2024 at Ukrainian Festival, BYOF ❤️</h3>
+            <h5>Which movie do you want to watch?</h5>
+            <ul>
+              <li>
+                <input
+                  type="checkbox"
+                  id="yesterday"
+                  value="Yesterday"
+                  onChange={handleCheckboxChange}
+                  checked={selectedMovies.includes('Yesterday')}
+                />
+                <label htmlFor="yesterday">Yesterday</label>
+              </li>
+              <li>
+                <input
+                  type="checkbox"
+                  id="armageddon"
+                  value="Armageddon"
+                  onChange={handleCheckboxChange}
+                  checked={selectedMovies.includes('Armageddon')}
+                />
+                <label htmlFor="armageddon">Armageddon</label>
+              </li>
+            </ul>
           </div>
         </div>
       )}
